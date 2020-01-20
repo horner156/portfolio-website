64161ed27f4fd18861f296649f8e0227e3ce395c
horner156 <60085767+horner156@users.noreply.github.com>
2020-01-20 15:34:50 -0500

    Added actual content to pages

---
+++ build/precache-manifest.fc5e4a7e2df01309d1fa1424f381baa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746648069c515fe451c9c44837a104ed",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "3c0a9f850474ac161052",
+    "url": "/portfolio-website/static/css/main.ae6f87fa.chunk.css"
+  },
+  {
+    "revision": "ca068862a34541dd3ce6",
+    "url": "/portfolio-website/static/js/2.8c9f9c3d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio-website/static/js/2.8c9f9c3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c0a9f850474ac161052",
+    "url": "/portfolio-website/static/js/main.42675abd.chunk.js"
+  },
+  {
+    "revision": "c1f91007e1c373e24be7",
+    "url": "/portfolio-website/static/js/runtime-main.a694116e.js"
+  }
+]);
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -7,14 +7,14 @@ return (
 	
 	
 		<h1>
-		Home Page Content
+		About Me
 		</h1>
 		
 		<p>
-		Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+		Hi, I'm Max. I started programming during my freshman year of highschool and as of right now I am 2 years from graduation at Ohio State. In highschool, I was president of the Computer Science Club,
+		 where we taught new students of Computer Science the basics of Java. I am now seeking an internship where more experienced programmers will offer me the same type of guidance.
+		 Most of my programming experience is in Java, but programming is a true passion of mine and I am dedicated to learning whatever it takes to be effective on the job. 
+		I have always strived for and attained academic excellence, and now that Computer Science is the basis of my education, I am dedicated to honing my skills in whatever ways possible.
 		</p>
 		
 		
